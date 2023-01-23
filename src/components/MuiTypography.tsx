7d512412c1f4702004982d4831f07a2f6e18065f
Tirth Parikh <tirthkparikh@gmail.com>
2023-01-23 15:14:09 +0530

    First commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,21 @@
+import { Typography } from "@mui/material"
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 Heading</Typography>
+      <Typography variant="h2">h2 Heading</Typography>
+      <Typography variant="h3">h3 Heading</Typography>
+      <Typography variant="h4" component="h1" gutterBottom>h4 Heading</Typography>
+      <Typography variant="h5">h5 Heading</Typography>
+      <Typography variant="h6">h6 Heading</Typography>
+      <Typography variant="subtitle1">subtitle1 Heading</Typography>
+      <Typography variant="subtitle2">subtitle2 Heading</Typography>
+      <Typography variant="body1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi modi commodi exercitationem unde, quod eum natus pariatur distinctio, ipsam sunt eius fuga id quisquam. Nam magni quo voluptates reiciendis molestiae?</Typography>
+      <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quaerat incidunt totam? Magni earum incidunt saepe dolorem, nesciunt doloremque neque quos repellat quibusdam laudantium reprehenderit rem facilis sunt, rerum ipsum!</Typography>
+      
+    </div>
+  )
+}
+
+export default MuiTypography
